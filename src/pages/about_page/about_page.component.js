bc2adeb2dab47984c3d2ea43dabf2a353cfa1f7e
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-07 10:25:33 +0800

    updated the contact Page and About us page CSS

--- src/pages/about_page/about_page.component.js
+++ src/pages/about_page/about_page.component.js
@@ -1,20 +1,32 @@
 import React from "react";
+import "./about_page.component.css";
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/images/app_logo.png"; //
 export class AboutPage extends React.Component {
   render() {
     return (
       <div>
+        <div className="headerAboutUs-container">
+          <Link to="/OverTheCounter">
+            <button className="backToHome-btn">Back To Home</button>
+          </Link>
+          <img className="bigLogo-container" src={logoImg} alt="" />
+        </div>
         <header className="about-us">
           <h2>About Us</h2>
         </header>
-        <body className="body-about">
-          Farmacia De Angat was made with the students from Bulacan State
-          University Bustos Bulacan. The project was created to help the
-          citizens of Bustos Bulacan to get buy their local medicine without
-          going out and risk their health in order to get the medicine. The Beta
-          Version only supported eleven (11) barangays namely; San roque, Santo.
-          Cristo, Santa. Cruz, Baybay, Banaban,
-          Marungko,Sulucan,Niugan,Taboc,Pulong yantok, and Engkanto
-        </body>
+
+        <div className="aboutPageBody-container">
+          <body className="body-about">
+            Farmacia De Angat was made with the students from Bulacan State
+            University Bustos Bulacan. The project was created to help the
+            citizens of Bustos Bulacan to get buy their local medicine without
+            going out and risk their health in order to get the medicine. The
+            Beta Version only supported eleven (11) barangays namely; San roque,
+            Santo. Cristo, Santa. Cruz, Baybay, Banaban,
+            Marungko,Sulucan,Niugan,Taboc,Pulong yantok, and Engkanto.
+          </body>
+        </div>
       </div>
     );
   }
--- src/pages/contact_page/contact_component.js
+++ src/pages/contact_page/contact_component.js
@@ -1,17 +1,29 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/images/app_logo.png";
+import "./contact_component.css";
 export class ContactPage extends React.Component {
   render() {
     return (
       <div>
-        <header className="header-ms">
-          <h4>Contact</h4>
+        <div className="headerAboutUs-container">
+          <Link to="/OverTheCounter">
+            <button className="backToHome-btn">Back To Home</button>
+          </Link>
+          <img className="bigLogo-container" src={logoImg} alt="" />
+        </div>
+        <header className="contactUs-header">
+          <h1 className="contactUs-txt">Contact Us</h1>
         </header>
-        <div className="body-contactPage">
-          <h2>Maynard L. Casimiro BSIT - 4C maynard.casimiropogi@gmail.com</h2>
-          <h2>Angelo Glenn V. Tigas BSIT 4C angeloglenn63@gmail.com</h2>
-          <h2>Bren Jei L.Simbulan BSIT-4C simbulanbrenjei0@gmail.com</h2>
-          <h2> Marian M. de Castro BSIT-4C decastromarian59@gmail.com</h2>
+        <div className="contactUs-container">
+          <body className="contactUs-body">
+            <h2>
+              Maynard L. Casimiro BSIT - 4C maynard.casimiropogi@gmail.com
+            </h2>
+            <h2>Angelo Glenn V. Tigas BSIT 4C angeloglenn63@gmail.com</h2>
+            <h2>Bren Jei L.Simbulan BSIT-4C simbulanbrenjei0@gmail.com</h2>
+            <h2> Marian M. de Castro BSIT-4C decastromarian59@gmail.com</h2>
+          </body>
         </div>
       </div>
     );
