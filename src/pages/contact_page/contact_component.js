8a8da337951c5c600b53f8e220bf2649996a66a1
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-07 09:44:34 +0800

    upadated contact page

--- src/pages/contact_page/contact_component.js
+++ src/pages/contact_page/contact_component.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export class ContactPage extends React.Component {
-	render() {
-		return (
-			<div>
-				<header className="header-ms">
-					<h1>Contact</h1>
-				</header>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <header className="header-ms">
+          <h4>Contact</h4>
+        </header>
+        <div className="body-contactPage">
+          <h2>Maynard L. Casimiro BSIT - 4C maynard.casimiropogi@gmail.com</h2>
+          <h2>Angelo Glenn V. Tigas BSIT 4C angeloglenn63@gmail.com</h2>
+          <h2>Bren Jei L.Simbulan BSIT-4C simbulanbrenjei0@gmail.com</h2>
+          <h2> Marian M. de Castro BSIT-4C decastromarian59@gmail.com</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactPage;
