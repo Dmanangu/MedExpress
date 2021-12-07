947f8dc86e485edc7aba3ca229b6cabce5209c70
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-07 09:48:34 +0800

    updated the about us page no CSS yet

--- src/pages/about_page/about_page.component.js
+++ src/pages/about_page/about_page.component.js
@@ -8,7 +8,12 @@ export class AboutPage extends React.Component {
         </header>
         <body className="body-about">
           Farmacia De Angat was made with the students from Bulacan State
-          University Bustos Bulacan.
+          University Bustos Bulacan. The project was created to help the
+          citizens of Bustos Bulacan to get buy their local medicine without
+          going out and risk their health in order to get the medicine. The Beta
+          Version only supported eleven (11) barangays namely; San roque, Santo.
+          Cristo, Santa. Cruz, Baybay, Banaban,
+          Marungko,Sulucan,Niugan,Taboc,Pulong yantok, and Engkanto
         </body>
       </div>
     );
