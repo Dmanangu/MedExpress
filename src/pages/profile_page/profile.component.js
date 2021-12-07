2fa89cb2aa2d6b8b2596d976f981e6b151258410
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-07 10:43:45 +0800

    updating user profile page

--- src/pages/profile_page/profile.component.js
+++ src/pages/profile_page/profile.component.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import { Navigation } from "../../component/export-components";
+import "./profile.component.css";
+import logoImg from "../../assets/images/app_logo.png";
 export class ProfilePage extends React.Component {
-	render() {
-		return <div />;
-	}
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div className="profile-container">
+          <h1 className="profile-text"> User Profile</h1>
+        </div>
+        <div className="userProfile-container">
+          <div></div>
+          <img className="userIcon" src={logoImg} alt="" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProfilePage;
