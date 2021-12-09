ab0553ab59e4a71e68110d5ce0a8b408133106bc
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-09 14:48:07 +0800

    modified the CSS structure

--- src/pages/covidessential_page/covidessential.component.js
+++ src/pages/covidessential_page/covidessential.component.js
@@ -1,92 +1,91 @@
-import React from 'react';
-import ceImg from '../../assets/images/bg_ce.png';
-import './covidessential.component.css';
+import React from "react";
+import ceImg from "../../assets/images/bg_ce.PNG";
+import "./covidessential.component.css";
 
-import store from '../../redux/store';
-import { Navigation } from '../../component/export-components';
-import { Card } from '../../component/product-card/card.component';
+import store from "../../redux/store";
+import { Navigation } from "../../component/export-components";
+import { Card } from "../../component/product-card/card.component";
 export class CovidEssentialPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			search: '',
-			results: [],
-			medicineList: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      results: [],
+      medicineList: [],
+    };
+  }
 
-	handleChange = (e, ownProps) => {
-		this.setState(
-			{
-				...this.state,
-				search: e.target.value
-			},
-			() => {
-				if (this.state.search && this.state.search.length >= 1) {
-					this.getResults(ownProps);
-				}
-			}
-		);
-	};
+  handleChange = (e, ownProps) => {
+    this.setState(
+      {
+        ...this.state,
+        search: e.target.value,
+      },
+      () => {
+        if (this.state.search && this.state.search.length >= 1) {
+          this.getResults(ownProps);
+        }
+      }
+    );
+  };
 
-	getResults = (ownProps) => {
-		//console.log(this.state.medicineList.map((e) => console.log(e)));
-		console.log('ORIGGGGGGGGGGGGGGGGG');
-		console.log(ownProps);
-		console.log('ORIGGGGGGGGGGGGGGGGG');
-		const filteredMedicine = ownProps.filter((meds) => {
-			return meds.prodName.toLowerCase().includes(this.state.search.toLocaleLowerCase());
-		});
-		console.log('FILETERRRR');
-		console.log(filteredMedicine);
-		console.log('FILETERRRR');
-		this.setState({
-			...this.state,
-			medicineList: filteredMedicine
-		});
-	};
+  getResults = (ownProps) => {
+    //console.log(this.state.medicineList.map((e) => console.log(e)));
+    console.log("ORIGGGGGGGGGGGGGGGGG");
+    console.log(ownProps);
+    console.log("ORIGGGGGGGGGGGGGGGGG");
+    const filteredMedicine = ownProps.filter((meds) => {
+      return meds.prodName
+        .toLowerCase()
+        .includes(this.state.search.toLocaleLowerCase());
+    });
+    console.log("FILETERRRR");
+    console.log(filteredMedicine);
+    console.log("FILETERRRR");
+    this.setState({
+      ...this.state,
+      medicineList: filteredMedicine,
+    });
+  };
 
-	render() {
-		const state = store.getState();
-		const medicine = state.requestRobots.medicine;
-		const medsByCategory = medicine.filter((meds) => {
-			return meds.category.toLowerCase().includes('d');
-		});
+  render() {
+    const state = store.getState();
+    const medicine = state.requestRobots.medicine;
+    const medsByCategory = medicine.filter((meds) => {
+      return meds.category.toLowerCase().includes("d");
+    });
 
-		console.log('LLLLLLLLLLLLLLLLLLLLLLLL');
-		console.log(this.state.medicineList.length);
-		console.log('LLLLLLLLLLLLLLLLLLLLLLLL');
+    console.log("LLLLLLLLLLLLLLLLLLLLLLLL");
+    console.log(this.state.medicineList.length);
+    console.log("LLLLLLLLLLLLLLLLLLLLLLLL");
 
-		//   <header className="header-pd">
-		//   <img className="pd-image" src={pdImg} alt="over" />
-		//   <div className="txt-padding" />
-		//   <h1 className="pd-txt">CovidEssentialPage Drugs</h1>
-		// </header>
-		return (
-			<div className="otc-content">
-				<Navigation />
-
-				<div className="search-container">
-					<div className="search">
-						<input
-							className="search-bar"
-							type="search"
-							//value={this.state.search}
-							placeholder="Search Medicine"
-							onChange={(e) => this.handleChange(e, medsByCategory)}
-						/>
-					</div>
-				</div>
-				<div className="contain-card">
-					{this.state.medicineList.length === 0 && this.state.search === '' ? (
-						medsByCategory.map((item) => <Card key={item.id} data={item} />)
-					) : (
-						this.state.medicineList.map((item) => <Card key={item.id} data={item} />)
-					)}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="otc-content">
+        <Navigation />
+        <header className="header-ce">
+          <img className="ce-image" src={ceImg} alt="over" />
+        </header>
+        <div className="CEsearch-container">
+          <div className="search">
+            <input
+              className="search-bar"
+              type="search"
+              //value={this.state.search}
+              placeholder="Search Covid Essentials Here"
+              onChange={(e) => this.handleChange(e, medsByCategory)}
+            />
+          </div>
+        </div>
+        <div className="contain-card">
+          {this.state.medicineList.length === 0 && this.state.search === ""
+            ? medsByCategory.map((item) => <Card key={item.id} data={item} />)
+            : this.state.medicineList.map((item) => (
+                <Card key={item.id} data={item} />
+              ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CovidEssentialPage;
