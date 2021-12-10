8d4493c42558366c4e88a602b370fb106ccbe8d0
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-10 16:48:55 +0800

    updated image borders and ongoing process for cart checkout

--- src/pages/checkout_page/checkout.component.jsx
+++ src/pages/checkout_page/checkout.component.jsx
@@ -2,41 +2,49 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { Navigation } from "../../component/navigation/navigation.component";
-
+import { useSelector } from "react-redux";
 import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import "./checkout.components.css";
+import { addItem } from "../../redux/cart/cart.actions";
 
-export const CheckoutPage = ({ props, cartItem, total }) => (
-  <div>
-    <Navigation />
-    <div className="checkout-page">
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
+export const CheckoutPage = ({ props, cartItem, total }) => {
+  const products = useSelector((state) => state);
+  console.log("products");
+  console.log(products);
+  console.log("products");
+  const medicine = products.requestRobots.medicine;
+  return (
+    <div>
+      <Navigation />
+      <div className="checkout-page">
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
         </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
+        {/* {medicine.map((cartItem) => (
+          <CheckoutPage key={props} addItem={addItem} />
+        ))} */}
+        <div className="total">TOTAL: ₱{total}</div>
       </div>
-      {/* {cartItem.map((cartItem) => (
-        <CheckoutItem key={props.data.id} cartItem={cartItem} />
-      ))} */}
-      <div className="total">TOTAL: ₱{total}</div>
     </div>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
